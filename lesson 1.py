84c73cdda412f2219cfc5e1d2bb9dfce4efb6590
Mariaade <122798094+Mariaade@users.noreply.github.com>
2023-03-08 18:07:36 +0000

    Add files via upload

---
+++ lesson 1.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 14 21:32:31 2022
+
+@author: maryt
+"""
+
+
+'''
+NOTES 
+
+The escape character \
+Double forward slash will return integer value //
+Whenever you want a new line  \n
+For indentation i.e tab               \t
+For space rather than new line
+
+'''
+
+
+# world = 52 
+
+# print(world)
+
+'''
+For decimal use / slash
+'''
+# print(7/3)
+
+'''
+To give just integer value use // slash
+'''
+#print(7//3)
+
+'''
+TO the power of use **
+'''
+# print(2**8)
+
+'''
+To print a string i.e. world, you must use double or single quotation "" OR ''
+Examples
+'''
+# print("This is a string")
+# print('Hello World')
+
+#
+''' 
+For new line, use \n
+E.G.
+'''
+#print('how are \nyou today')
+
+
+
+''' 
+For new  indentation use \t
+E.G.
+'''
+#print('how are \tyou today')
+#
+'''
+For new line and indentation use \n\t
+E.G.
+'''
+#print('how are \n\tyou today')
+
+
+'''
+To escape a character, use \... check the example below for better understanding
+'''
+
+# print('how\'s it going?')
+# 
+# print("how's is it going today?")
+'''
+More examples
+'''
+
+# print("one")
+# print("two")
+# print("three")
+# print("four")
+
+'''
+Another way in other to be on a line is to use end=''
+'''
+
+# print("one", end=' ')
+# print("two", end=' ')
+# print("three", end=' ')
+# print("four", end=' ')
+
+
+
+# print("one ,two ")
+
+# print("Hello")
+# print('hello')
+
+
+
+
+
+
+
+
+#print('one,\'two\', \"three\", four,\\ five\\\n\t Once, \'I\' caught a\
+      fish\n\t\t'//alive' ')
+          
+      
+        
+      
+        
+      
+        
+      
+        
+      
+        
+      
+        
+      
+        
+      
